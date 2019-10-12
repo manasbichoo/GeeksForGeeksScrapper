4952fc56974e3e70372851b0cd32b44097ebc607
manasbichoo <manas16b@gmail.com>
2019-10-12 18:22:13 +0530

    Added user agent strings to make the project more reliable and secure

--- g4g.py
+++ g4g.py
@@ -14,6 +14,8 @@ from article import Article
 
 BASE_URL = "http://www.geeksforgeeks.org/"
 articles = []
+headers = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
 CHOICE_TO_CATEGORY_MAPPING = {
     1: "c",
